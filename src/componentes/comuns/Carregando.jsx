209fe0b4c490ff5a4b0fc41ecce1e3768873bee3
Luiz Felipe Fornari <luizfelipefornari@GMAIL.COM>
2024-05-02 16:37:13 -0300

    fixing bugs

--- src/componentes/comuns/Carregando.jsx
+++ src/componentes/comuns/Carregando.jsx
@@ -2,8 +2,15 @@ import Spinner from "react-bootstrap/Spinner";
 
 function Carregando(props) {
   const indicatorSize = 80;
+  const divStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh", // Opcional: define a altura da div para ocupar toda a altura da tela
+  };
+
   return (
-    <>
+    <div style={divStyle}>
       {!props.carregando ? (
         props.children
       ) : (
@@ -11,7 +18,7 @@ function Carregando(props) {
           <span className="visually-hidden">Loading...</span>
         </Spinner>
       )}
-    </>
+    </div>
   );
 }
 
