ea3ed948b1a2f1d755d02e4aeec2aef72264735c
Luiz Felipe Fornari <luizfelipefornari@GMAIL.COM>
2024-05-02 16:45:41 -0300

    fixing bugs

--- src/componentes/comuns/Carregando.jsx
+++ src/componentes/comuns/Carregando.jsx
@@ -2,6 +2,7 @@ import Spinner from "react-bootstrap/Spinner";
 
 function Carregando(props) {
   const indicatorSize = 80;
+
   const divStyle = {
     display: "flex",
     justifyContent: "center",
@@ -10,13 +11,15 @@ function Carregando(props) {
   };
 
   return (
-    <div style={divStyle}>
+    <div>
       {!props.carregando ? (
         props.children
       ) : (
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
+        <div style={divStyle}>
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
       )}
     </div>
   );
